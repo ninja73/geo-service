8285dd43cc7f65b76f2b3bff28e534b6523c4a12
vladimir.danilov <dank73@inbox.ru>
2018-06-24 13:36:01 +0400

    Add data loader

--- src/main/scala/geo/Boot.scala
+++ src/main/scala/geo/Boot.scala
@@ -3,61 +3,50 @@ package geo
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.server.Route
 import akka.stream.ActorMaterializer
 import com.typesafe.scalalogging.LazyLogging
-import geo.Message.{CellSearchException, Config, GridCell, LocationTag}
+import geo.config.Config
+import geo.entity.Entity._
 import geo.route.CustomExceptionHandler._
 import geo.route.GeoRoute
-import geo.store.Storage
-import geo.util.{GridCellParser, GridCellTransform, LocationTagParser, LocationTagTransform}
+import geo.store.InMemoryStorage
+import geo.util._
 
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, ExecutionContext}
 
 /** Пример запуска проекта:
   * sbt "run
-  *     -u ./src/main/resources/user_labels.txt
-  *     -g ./src/main/resources/user_labels.txt
-  *     -c 4421353 --min-tile-x -30 --min-tile-y -90 --max-tile-x 30 --max-tile-y 90"
+  * -u ./src/main/resources/user_labels.txt
+  * -g ./src/main/resources/user_labels.txt
   * */
 object Boot extends App with LazyLogging {
 
-  ConfigParser.parser.parse(args, Config()) match {
+  ConfigParser.parser.parse(args, CmdConfig()) match {
     case Some(config) ⇒
       implicit val system: ActorSystem = ActorSystem.create("geo-service")
       implicit val materializer: ActorMaterializer = ActorMaterializer.create(system)
       implicit val dispatcher: ExecutionContext = system.dispatcher
 
-      val calculateUserLabelIndex: Int ⇒ Int = i ⇒ i - 1
-
-      val calculateGridIndex: ((Int, Int)) ⇒ Int = tile ⇒ {
-        val (x, y) = tile
-        if (x < config.gridMinTileX || x > config.gridMaxTileX || y < config.gridMinTileY || y > config.gridMaxTileY)
-          throw CellSearchException()
-        else
-          (x - config.gridMinTileX) * (config.gridMinTileY.abs + config.gridMaxTileY) + (y - config.gridMinTileY)
-      }
-
-      val userLabelStore = new Storage[LocationTag, Int](
-        fileName = config.userLabelsPath,
-        chunkSize = 99,
-        lastIndex = config.userLabelCount,
-        calculateIndex = calculateUserLabelIndex)
+      val userLabelLoader = new LoadData[LocationTag](config.userLabelsPath)
         with LocationTagTransform with LocationTagParser
+      val gridLoader = new LoadData[Grid](config.gridPath)
+        with GridTransform with GridParser
 
-      val geographicGrid = new Storage[GridCell, (Int, Int)](
-        fileName = config.gridPath,
-        chunkSize = 132,
-        lastIndex = 0,
-        calculateIndex = calculateGridIndex)
-        with GridCellTransform with GridCellParser
+      val userStorage = InMemoryStorage[Long, LocationTag]()
+      val gridStorage = InMemoryStorage[GridId, Grid]()
 
-      val router: Route = handleExceptions(exceptionHandler) {
-        GeoRoute(userLabelStore, geographicGrid).route
+      val bindingFuture = for {
+        _ ← userLabelLoader.load(tag ⇒ userStorage.update(tag.userId, tag))
+        _ ← gridLoader.load(g ⇒ gridStorage.update(g.id, g))
+        router = handleExceptions(exceptionHandler) {
+          GeoRoute(userStorage, gridStorage).route
+        }
+        bind ← Http().bindAndHandle(router, Config.webServer.host, Config.webServer.port)
+      } yield {
+        logger.info(s"Started host: ${Config.webServer.host}, port: ${Config.webServer.port}")
+        bind
       }
-
-      val bindingFuture = Http().bindAndHandle(router, "localhost", 9090)
 
       Await.ready(system.whenTerminated, Duration.Inf)
       bindingFuture
---
+++ src/main/scala/geo/util/LoadData.scala
@@ -0,0 +1,36 @@
+package geo.util
+
+import java.nio.file.Paths
+
+import akka.stream.scaladsl.{FileIO, Flow, Framing, Keep, Sink, Source}
+import akka.stream.{IOResult, Materializer}
+import akka.util.ByteString
+import akka.{Done, NotUsed}
+import geo.entity.Entity
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class LoadData[T <: Entity](fileName: String)
+                           (implicit materializer: Materializer, dispatcher: ExecutionContext) {
+  self: Transformer[T] with BaseParsers[T] ⇒
+
+  val lineDelimiter: Flow[ByteString, ByteString, NotUsed] =
+    Framing.delimiter(ByteString("\n"), 9000)
+
+  def load(f: T ⇒ Unit): Future[Done] = {
+    FileIO.fromPath(Paths.get(fileName))
+      .via(lineDelimiter)
+      .filter(_.nonEmpty)
+      .mapAsync(4)(b ⇒ Future.successful(readObj(b.utf8String)))
+      .collect { case Some(t) ⇒ t }
+      .runWith(Sink.foreachParallel(4)(f))
+  }
+
+  private def append: Sink[String, Future[IOResult]] =
+    Flow[String]
+      .map(str ⇒ ByteString(s"$str$Eol"))
+      .toMat(FileIO.toPath(Paths.get(fileName)))(Keep.right)
+
+  def save(m: Map[Long, T]): Future[IOResult] =
+    Source[String](m.values.map(toStorage).toIndexedSeq).runWith(append)
+}
